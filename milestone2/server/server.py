8f32b573dfe187b467169a68467f600fec3a10d4
EpureRares <rares_stefan1999@yahoo.ro>
2024-04-15 17:55:10 +0200

    running database

---
+++ milestone2/server/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import pymongo
+import os
+from pymongo import MongoClient
+
+def get_db():
+    client = MongoClient(host=os.environ['MONGODB_HOSTNAME'],
+                         port=27017, 
+                         username=os.environ['MONGODB_USERNAME'], 
+                         password=os.environ['MONGODB_PASSWORD'],
+                        authSource="admin")
+    return client
+
+
+app = Flask(__name__)
+client_db = get_db()
+
+@app.route('/')
+def ping_server():
+    return "Welcome to Data Viz."
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)

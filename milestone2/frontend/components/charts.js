82eb0b6d476f52336a47a2dea3ce1ad89ea3eae0
ChristianB024 <b0tzki.christian@gmail.com>
2024-04-25 13:04:14 +0200

    Timeseries viz

--- milestone2/frontend/components/charts.js
+++ milestone2/frontend/components/charts.js
@@ -1,3 +1,7 @@
+function removeTimeComponent(date) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
+
 export async function loadTimeSeriesChart(containerId, dates, counts) {
     // Convert date strings to JavaScript Date objects
     const parseDate = d3.timeParse("%m/%d/%Y");
@@ -6,124 +10,143 @@ export async function loadTimeSeriesChart(containerId, dates, counts) {
         count: counts[index]
     }));
 
-    const firstTenElements = formattedData.slice(0, 10);
-    const firstTenDates = dates.slice(0, 10);
-    console.log('Printam aici debug')
-    console.log(firstTenElements);
-    console.log('Printam aici debug')
-    console.log(firstTenDates);
+    const minDate = new Date(Math.min(...formattedData.map((data) => data.date)));
+    const maxDate = new Date(Math.max(...formattedData.map((data) => data.date)));
 
+    const minDateString = minDate.toISOString().split('T')[0];
+    const maxDateString = maxDate.toISOString().split('T')[0];
 
-    // Set up dimensions
-
-
-    const container = document.getElementById(`chart-container`);
-    console.log(container)
-    const parentWidth = container.clientWidth;
-    const parentHeight = window.innerHeight - container.offsetTop; // Adjusted for page scroll and header height
-
-    const margin = { top: 20, right: 30, bottom: 80, left: 60 };
-    const width = parentWidth - margin.left - margin.right;
-    const height = parentHeight - margin.top - margin.bottom;
-
-    // Create SVG element
-    const svg = d3.select(`#${containerId}`)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    // Create scales
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(formattedData, d => d.date))
-        .range([0, width]);
-
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(formattedData, d => d.count)])
-        .nice()
-        .range([height, 0]);
-
-    // Create line generator
-    const line = d3.line()
-        .x(d => xScale(d.date))
-        .y(d => yScale(d.count))
-        .curve(d3.curveMonotoneX);
-
-    // Draw line
-    svg.append("path")
-        .datum(formattedData)
-        .attr("fill", "none")
-        .attr("stroke", "rgb(75, 192, 192)")
-        .attr("stroke-width", 2)
-        .attr("d", line);
-
-    // Draw circles for data points
-    svg.selectAll(".dot")
-        .data(formattedData)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("cx", d => xScale(d.date))
-        .attr("cy", d => yScale(d.count))
-        .attr("r", 5)
-        .attr("fill", "rgb(75, 192, 192)")
-        .attr("stroke", "white")
-        .attr("stroke-width", 2);
-
-    // Add axis labels
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height + margin.top + 20)
-        .text("Date");
-
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left + 20)
-        .attr("x", -margin.top)
-        .text("Occurrences");
-
-    // Define axis generators
-    const xAxis = d3.axisBottom(xScale).ticks(5).tickFormat(d3.timeFormat("%Y-%m-%d"));
-    const yAxis = d3.axisLeft(yScale).ticks(5);
-
-    // Draw axes
-    svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0, ${height})`)
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y-axis")
-        .call(yAxis);
+    createChart(formattedData);
 
     // Get the input elements
-    const startDateInput = document.getElementById("start-date");
-    const endDateInput = document.getElementById("end-date");
+    const startDateInput = document.getElementById('start-date');
+    const endDateInput = document.getElementById('end-date');
+    startDateInput.value = minDateString;
+    endDateInput.value = maxDateString;
+
+    let selectedStartDate = new Date(startDateInput.value);
+    let selectedEndDate = new Date(endDateInput.value);
 
     // Add input event listeners to update the chart
-    startDateInput.addEventListener("input", updateChart);
-    endDateInput.addEventListener("input", updateChart);
+    startDateInput.addEventListener('input', (e) => {
+        selectedStartDate = removeTimeComponent(new Date(e.target.value));
+        const filteredData = formattedData.filter((d) => {
+            const dateToCompare = removeTimeComponent(d.date);
+            return (
+                selectedStartDate <= dateToCompare && dateToCompare <= selectedEndDate
+            );
+        });
 
-    function updateChart() {
-        const startDate = new Date(startDateInput.value);
-        const endDate = new Date(endDateInput.value);
-        const filteredData = formattedData.filter(d => d.date >= startDate && d.date <= endDate);
+        createChart(filteredData);
+    });
+    endDateInput.addEventListener('input', (e) => {
+        selectedEndDate = removeTimeComponent(new Date(e.target.value));
+        const filteredData = formattedData.filter((d) => {
+            const dateToCompare = removeTimeComponent(d.date);
+            return (
+                selectedStartDate <= dateToCompare && dateToCompare <= selectedEndDate
+            );
+        });
 
-        xScale.domain(d3.extent(filteredData, d => d.date));
-        yScale.domain([0, d3.max(filteredData, d => d.count)]);
+        createChart(filteredData);
+    });
 
-        svg.select(".x-axis").call(xAxis);
-        svg.select(".y-axis").call(yAxis);
+    function createChart(data) {
+        data.sort((a, b) => (a.date < b.date ? -1 : 1));
 
-        svg.select(".line")
-            .datum(filteredData)
-            .attr("d", line);
+        // Set up dimensions
+        const container = document.getElementById(`chart-container`);
+        const parentWidth = container.clientWidth;
+        const parentHeight = window.innerHeight - container.offsetTop; // Adjusted for page scroll and header height
 
-        svg.selectAll(".dot")
-            .data(filteredData)
-            .attr("cx", d => xScale(d.date))
-            .attr("cy", d => yScale(d.count));
+        const margin = { top: 20, right: 30, bottom: 20, left: 30 };
+        const width = parentWidth - margin.left - margin.right;
+        const height = parentHeight - margin.top - margin.bottom;
+
+        // Clear the svg container to draw a chart from scratch
+        d3.selectAll('g > *').remove();
+
+        // Create SVG element
+        const svg = d3
+            .select(`#${containerId}`)
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', `translate(${margin.left},${margin.top})`);
+
+        // Create scales
+        const xScale = d3
+            .scaleTime()
+            .domain(d3.extent(data, (d) => d.date))
+            .range([0, width]);
+
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, d3.max(data, (d) => d.count)])
+            .nice()
+            .range([height, 0]);
+
+        // Add axis labels
+        svg
+            .append('text')
+            .attr('text-anchor', 'end')
+            .attr('x', width)
+            .attr('y', height + margin.top + 20)
+            .text('Date');
+
+        svg
+            .append('text')
+            .attr('text-anchor', 'end')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', -margin.left + 20)
+            .attr('x', -margin.top)
+            .text('Occurrences');
+
+        // Define axis generators
+        const xAxis = d3
+            .axisBottom(xScale)
+            .ticks(5)
+            .tickFormat(d3.timeFormat('%Y-%m-%d'));
+        const yAxis = d3.axisLeft(yScale).ticks(5);
+
+        // Draw axes
+        svg
+            .append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', `translate(0, ${height})`)
+            .call(xAxis);
+
+        svg.append('g').attr('class', 'y-axis').call(yAxis);
+
+        // Create line generator
+        const line = d3
+            .line()
+            .x((d) => xScale(d.date))
+            .y((d) => yScale(d.count))
+            .curve(d3.curveMonotoneX);
+
+        // Draw line
+        svg
+            .append('path')
+            .datum(data)
+            .attr('fill', 'none')
+            .attr('stroke', 'rgb(75, 192, 192)')
+            .attr('stroke-width', 2)
+            .attr('d', line);
+
+        // Draw circles for data points
+        svg
+            .selectAll('.dot')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', 'dot')
+            .attr('cx', (d) => xScale(d.date))
+            .attr('cy', (d) => yScale(d.count))
+            .attr('r', 5)
+            .attr('fill', 'rgb(75, 192, 192)')
+            .attr('stroke', 'white')
+            .attr('stroke-width', 2);
     }
 }
--- milestone2/frontend/main.js
+++ milestone2/frontend/main.js
@@ -1,16 +1,37 @@
-import { loadTimeSeriesData } from "./components/load.js";
-import { loadTimeSeriesChart } from "./components/charts.js";
-import {timeSeriesViz} from "./components/time_series.js";
+import { loadTimeSeriesData } from './components/load.js';
+import { loadTimeSeriesChart } from './components/charts.js';
+// import { timeSeriesViz } from './components/time_series.js';
 
 async function init() {
     // const ctx = document.getElementById('myChart').getContext('2d');
     try {
         const { dates, counts } = await loadTimeSeriesData();
-        loadTimeSeriesChart("myChart", dates, counts);
+        loadTimeSeriesChart('myChart', dates, counts);
         // timeSeriesViz(ctx, dates, counts);
+
+        const selectDropdown = document.getElementById('ds-select');
+
+        selectDropdown.addEventListener('change', function () {
+            const selectedValue = selectDropdown.value;
+
+            switch (selectedValue) {
+                case 'ds1':
+                    console.log('selected ds1');
+                    loadTimeSeriesChart('myChart', dates, counts);
+                    break;
+                case 'ds2':
+                    console.log('selected ds2');
+                    loadTimeSeriesChart('myChart', dates, counts);
+                    break;
+                case 'ds3':
+                    console.log('selected ds3');
+                    loadTimeSeriesChart('myChart', dates, counts);
+                    break;
+            }
+        });
     } catch (error) {
         console.error('Error initializing chart:', error);
     }
 }
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init);
